c40625c39658c78cb95e3a6e30bea54f79a7c816
Gyeongjae Choi <def6488@gmail.com>
2020-12-22 11:22:45 +0900

    Create jiggle.ps1

---
+++ tools/jiggle.ps1
@@ -0,0 +1,9 @@
+param($minutes = 60)
+
+$myshell = New-Object -com "Wscript.Shell"
+
+for ($i = 0; $i -lt $minutes; $i++) {
+  Start-Sleep -Seconds 60
+  $myshell.sendkeys("{SCROLLLOCK}")
+  $myshell.sendkeys("{SCROLLLOCK}")
+}
